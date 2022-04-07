622316d379addf5ac7002d43acc97991bb44108d
Hyrum Coleman <99374726+Hyrum-Coleman@users.noreply.github.com>
2022-04-07 14:47:05 -0600

    moved Quadratic to sleepCalculator

--- sleepCalculator.m
+++ sleepCalculator.m
@@ -1,3 +1,17 @@
+% Sleep calculator script
+
+
+gaming = 2;
+trolling = 3;
+malding = -1;
+
+sleep = Quadratic(gaming, trolling, malding);
+
+
+
+
+
+
 function [posRoot, negRoot] = Quadratic(a, b, c)
 if((b .^ 2) - (4 .* a .* c) >= 0 ) 
     posRoot = (-b + sqrt(b.^2 - 4 .* a .* c)) ./ (2 .* a);
