c44dc58344b0171f4fbd42673a835c6d0a0c7460
Hyrum Coleman <99374726+Hyrum-Coleman@users.noreply.github.com>
2022-04-07 14:05:57 -0600

    Create Quadratic.m

---
+++ Quadratic.m
@@ -0,0 +1,8 @@
+function [posRoot, negRoot] = Quadratic(a, b, c)
+if((b .^ 2) - (4 .* a .* c) >= 0 ) 
+    posRoot = (-b + sqrt(b.^2 - 4 .* a .* c)) ./ (2 .* a);
+    negRoot = (-b - sqrt(b.^2 - 4 .* a .* c)) ./ (2 .* a);
+else
+    error("Roots are complex, aborting program") 
+end
+end
