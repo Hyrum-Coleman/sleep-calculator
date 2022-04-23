35c775cda54ee1501d353cc095a009d74731976c
Hyrum Coleman <99374726+Hyrum-Coleman@users.noreply.github.com>
2022-04-22 21:33:02 -0600

    added timeVal

--- sleepCalculator.m
+++ sleepCalculator.m
@@ -4,7 +4,7 @@ clear; clc; close all;
 
 amountOfHoursRemaining = input("How many hours of estimated work do you have remaining? ");
 amountOfTasksToComplete = input("How many assignments are remaining? ");
-hoursSinceLastSleep = input("How long has it been since you last slept? ");
+hoursSinceLastSleep = input("How many hours has it been since you last slept? ");
 time = clock;
 fix(time);
 currentMinute = time(5);
@@ -14,7 +14,9 @@ fplot(sleepE, [0, 5]);
 
 
 %Combine tasks and hours of work
+workVal = TasksAndHours(amountOfHoursRemaining, amountOfTasksToComplete);
 % Combine current hour and hours since last sleep
+timeVal = SleepAndTime(hoursSinceLastSleep, currentHour);
 % Constrain workVal and timeVal
 
 % if statement with workVal and timeVal directing to options
@@ -27,6 +29,7 @@ fplot(sleepE, [0, 5]);
 
 
 function [posRoot, negRoot] = Quadratic(a, b, c)
+
 if((b .^ 2) - (4 .* a .* c) >= 0 ) 
     posRoot = (-b + sqrt(b.^2 - 4 .* a .* c)) ./ (2 .* a);
     negRoot = (-b - sqrt(b.^2 - 4 .* a .* c)) ./ (2 .* a);
@@ -39,6 +42,22 @@ end
 function workVal = TasksAndHours(amountOfHours, amountOfTasks)
 
 workVal = amountOfHours * amountOfTasks - (1/sqrt(amountOfHours * amountOfTasks + 1));
+
+end
+
+function timeVal = SleepAndTime(timeSinceLastSleep, currentTime)
+
+% So currentTime will vary between 0-23 or 1-24 idk but it's like that
+% time sinceLastSleep will be between 0-24
+% I want timeVal to be be big (bad) if timeSinceLastSleep is large
+
+% If currentTime is between 22-6 then the score should remain very big
+if ((currentTime <= 6) || (currentTime >= 22))
+    timeVal = timeSinceLastSleep;
+% if current time is any other value it should go to the mid range
+else
+    timeVal = timeSinceLastSleep - 10;
+end
 
 end
 
